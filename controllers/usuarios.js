c38f12a5c9bf0d05f40497422deea81c48904e0d
bryan <bryansugi@gmail.com>
2021-05-20 10:29:23 -0500

    despliegue app

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,30 +1,94 @@
 const { response } = require('express')
+const bcryptjs = require('bcryptjs')
 
-const usuariosGet = (req, res = response) => {
+const Usuario = require('../models/usuario')
 
-    const { hobby = 'No hobby', gustos = 'No gustos' } = req.query
+const usuariosGet = async(req, res = response) => {
+
+    const { limite = 5, desde = 0, id = '' } = req.query
+
+    /*  const [total, usuarios] = await Promise.all([
+
+         Usuario.countDocuments({ estado: true }),
+
+         Usuario.find({ estado: true })
+         .skip(Number(desde))
+         .limit(Number(limite))
+
+     ]) */
+
+    let usuarios = ''
+    const total = await Usuario.countDocuments({ estado: true })
+    if (id) {
+        usuarios = await Usuario.findById(id)
+    } else {
+        usuarios = await Usuario.find({ estado: true })
+            .skip(Number(desde))
+            .limit(Number(limite))
+    }
 
     res.json({
-        msg: 'get de mi api -controller',
-        hobby,
-        gustos
+        total,
+        usuarios
     })
 }
 
-const usuariosPost = (req, res = response) => {
-    const { nombre, edad } = req.body
-    res.status(201).json({
-        msg: 'post de mi api -controller',
-        nombre,
-        edad
+const usuariosPost = async(req, res = response) => {
+
+
+    const { nombre, correo, password, rol } = req.body
+
+    const usuario = new Usuario({ nombre, correo, password, rol })
+
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync()
+    usuario.password = bcryptjs.hashSync(password, salt)
+
+
+    //se guarda en la BD
+    await usuario.save()
+
+
+    res.json({
+        usuario
     })
 }
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async(req, res = response) => {
+
     const { id } = req.params;
-    res.status(500).json({
-        msg: 'put de mi api -controller',
-        id
+    const { _id, password, google, correo, ...resto } = req.body;
+
+    //TODO validar contra base de datos
+    if (password) {
+
+        const salt = bcryptjs.genSaltSync()
+        resto.password = bcryptjs.hashSync(password, salt)
+
+    }
+
+    //Este es el codigo que realiza la actualizacion el la base de datos
+    const usuarioDB = await Usuario.findByIdAndUpdate(id, resto)
+
+
+    res.json({
+        usuarioDB
+    })
+}
+
+
+const usuariosDelete = async(req, res = response) => {
+    const { id } = req.params;
+
+    //fisicamente lo borramos
+    //const usuario = await Usuario.findByIdAndDelete(id)
+
+
+    const usuario = await Usuario.findByIdAndUpdate(id, { estado: false })
+
+
+    res.json({
+        usuario
     })
 }
 
@@ -33,15 +97,6 @@ const usuariosPatch = (req, res = response) => {
         msg: 'patch de mi api -controller'
     })
 }
-
-const usuariosDelete = (req, res = response) => {
-    const { id } = req.params;
-    res.json({
-        msg: 'delete de mi api -controller',
-        id
-    })
-}
-
 module.exports = {
     usuariosGet,
     usuariosPost,
---
+++ helpers/db-validators.js
@@ -0,0 +1,36 @@
+const Role = require('../models/role')
+const Usuario = require('../models/usuario')
+
+const esRolValido = async(rol = '') => {
+    const existeRol = await Role.findOne({ rol })
+    if (!existeRol) {
+        throw new Error(`El rol ${rol} no está registrado en la BD`)
+    }
+
+}
+
+//validar si el correo existe
+const emailExiste = async(correo = '') => {
+
+    const existeEmail = await Usuario.findOne({ correo })
+    if (existeEmail) {
+        throw new Error(`El correo ${correo} ya existe en la BD`)
+    }
+
+}
+
+const existeUsuarioPorId = async(id) => {
+
+    const existeUsuario = await Usuario.findById(id)
+    if (!existeUsuario) {
+        throw new Error(`El id ${id} no existe en la BD`)
+    }
+
+}
+
+
+module.exports = {
+    esRolValido,
+    emailExiste,
+    existeUsuarioPorId
+}
